74b633fb6ca9fd7154ab242b5e2a755f8b4be471
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2026-09-07 13:28:58 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GenAlgLab_1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
