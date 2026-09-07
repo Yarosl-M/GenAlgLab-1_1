7cb6908084c7483ccb0fe75755d57d4fb1ae4df0
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2026-09-07 13:49:41 +0300

    Add GeneticFunction and GeneticInstance classes

---
+++ GeneticFunction.cs
@@ -0,0 +1,21 @@
+﻿namespace GenAlgLab_1_1
+{
+    public interface IGeneticFunction
+    {
+        public double MinX { get; }
+        public double MaxX { get; }
+        public double Get(double x);
+    }
+    public class GeneticFunction : IGeneticFunction
+    {
+        public double MinX { get; } = 0.0;
+        public double MaxX { get; } = 1.0;
+        public double Get(double x)
+        {
+            // currently extremes are:
+            // maximum at (0.3171 1.0892)
+            // minimum at (0.807 -0.9563)
+            return Math.Sin(6 * x - 1) + Math.Cos(4 * x) + 2 * Math.Pow(x, 5);
+        }
+    }
+}
---
+++ GeneticInstance.cs
@@ -0,0 +1,30 @@
+﻿namespace GenAlgLab_1_1
+{
+    public class GeneticInstance
+    {
+        public IGeneticFunction Function { get; set; }
+        public double Value { get; set; }
+        public UInt64 ValueEncoded
+        {
+            // this is actually bad because we don't have this encoding as the "principal" one
+            // it's not stored in memory
+            get
+            {
+                // this is just a ratio of the segment lengths
+                // should be between 0.0 and 1.0
+                double percentage = (Value - Function.MinX) / (Function.MaxX - Function.MinX);
+                // just multiplies the max 64-bit int value by this amount
+                UInt64 mapped_value = (UInt64)(percentage * (double)UInt64.MaxValue) ;
+                return mapped_value;
+            }
+            set
+            {
+                // the inverse operation
+                // ratio between a new val and the max val
+                double percentage = (double)value / (double)UInt64.MaxValue;
+                Value = percentage * (Function.MaxX - Function.MinX) + Function.MinX;
+                Value = percentage * (Function.MaxX - Function.MinX) + Function.MinX;
+            }
+        }
+    }
+}
