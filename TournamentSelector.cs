54520db19ffcee9daa4cffa03d6bb1c3061f9b3d
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2026-09-09 16:27:06 +0300

    Create TournamentSelector.cs
    nothing to see here for now

---
+++ TournamentSelector.cs
@@ -0,0 +1,13 @@
+﻿
+namespace GenAlgLab_1_1
+{
+    public class TournamentSelector : IGeneticSelector
+    {
+        
+        IEnumerable<GeneticInstance> IGeneticSelector.Select(
+            IEnumerable<GeneticInstance> instances, int count, int? seed)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
