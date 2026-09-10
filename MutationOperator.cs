1694d4e9439cb006b4c7d888cb535b599125ed8f
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2026-09-10 11:16:19 +0300

    Add MutationOperator

---
+++ MutationOperator.cs
@@ -0,0 +1,24 @@
+﻿namespace GenAlgLab_1_1
+{
+
+    public static class MutationOperator
+    {
+        /// <summary>
+        /// Выполняет случайную мутацию в хромосоме экземпляра (изменяет count случайных битов).
+        /// </summary>
+        /// <param name="instance">Экземпляр особи, мутацию которого необходимо выполнить.</param>
+        /// <param name="count">Количество мутаций за один раз (мутации могут выполняться на
+        /// одном и том же бите, поэтому фактически будет меньше изменений)</param>
+        /// <param name="rng">Необязательно — объект генератора случайных чисел
+        /// для обеспечения повторяемости результатов</param>
+        public static void Mutate(GeneticInstance instance, int count = 1, Random? rng = null)
+        {
+            if (rng is null) rng = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                UInt64 flip_mask = 1UL << rng.Next(0, 64);
+                instance.Value ^= flip_mask;
+            }
+        }
+    }
+}
