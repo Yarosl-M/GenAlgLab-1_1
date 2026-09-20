ef3007683e08e0e5dff3ae89a26d79a2baea8935
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2026-09-20 19:42:07 +0300

    Refactor GeneticFunction

--- GeneticAlgorithm/GeneticFunction.cs
+++ GeneticAlgorithm/GeneticFunction.cs
@@ -1,24 +1,24 @@
 ﻿namespace GenAlgLab_1_1
 {
-    public interface IGeneticFunction
+    public class GeneticFunction
     {
-        public double MinX { get; }
-        public double MaxX { get; }
-        public double Get(double x);
-        // x-координата экстремума, к которому должен сходиться алгоритм
-        public double ExtremumX { get; }
-    }
-    public class GeneticFunction : IGeneticFunction
-    {
-        public double MinX { get; } = 0.0;
-        public double MaxX { get; } = 1.0;
-        // currently extremes are:
+        public double MinX { get; init; }
+        public double MaxX { get; init; }
+        // OLD FUNCTION, extremes are:
         // maximum at (0.3171 1.0892)
         // minimum at (0.807 -0.9563)
-        public double ExtremumX { get; } = 0.3171;
-        public double Get(double x)
+        public IEnumerable<double> ExtremumX { get; init; }
+        public Func<double, double> Function { get; init; }
+        //public double Get(double x)
+        //{
+        //    return Math.Sin(6 * x - 1) + Math.Cos(4 * x) + 2 * Math.Pow(x, 5);
+        //}
+        public double Get(double x) => Function(x);
+
+        public GeneticFunction(double min_x,  double max_x, IEnumerable<double> extremes,
+            Func<double, double> function)
         {
-            return Math.Sin(6 * x - 1) + Math.Cos(4 * x) + 2 * Math.Pow(x, 5);
+            MinX = min_x; MaxX = max_x; ExtremumX = extremes; Function = function;
         }
     }
 }
