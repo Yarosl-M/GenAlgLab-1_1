2766a185a3a93fc12e59bacb4282cbb592678d05
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2026-09-23 20:47:21 +0300

    Refactor MutationOperator

--- GeneticAlgorithm/MutationOperator.cs
+++ GeneticAlgorithm/MutationOperator.cs
@@ -9,17 +9,20 @@ namespace GenAlgLab_1_1
         /// Выполняет случайную мутацию в хромосоме экземпляра (изменяет count случайных битов).
         /// </summary>
         /// <param name="instance">Экземпляр особи, мутацию которого необходимо выполнить.</param>
-        /// <param name="count">Количество мутаций за один раз (мутации могут выполняться на
+        /// <param name="mutation_count">Количество мутаций за один раз (мутации могут выполняться на
         /// одном и том же бите, поэтому фактически будет меньше изменений).</param>
         /// <param name="rng">Необязательно — объект генератора случайных чисел
         /// для обеспечения повторяемости результатов.</param>
-        public void Mutate(GeneticInstance instance, int count = 1, Random? rng = null)
+        public void Mutate(GeneticInstance instance, int mutation_count = 1, Random? rng = null)
         {
             if (rng is null) rng = new Random();
-            for (int i = 0; i < count; i++)
-            {
-                UInt64 flip_mask = 1UL << rng.Next(0, 64);
-                instance.Value ^= flip_mask;
+            for (int j = 0; j < instance.ParameterCount; j++)
+            { // do a mutation for each of the genes independently
+                for (int i = 0; i < mutation_count; i++)
+                {
+                    UInt64 flip_mask = 1UL << rng.Next(64);
+                    instance.Value[j] ^= flip_mask;
+                }
             }
         }
     }
